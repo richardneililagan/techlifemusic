57cfd02db7f17ab187cc260e25cf60518e4b063a
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-04-09 13:57:07 +0800

    Adds attribution to sidebar.

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -1,15 +1,40 @@
 // @flow strict
-import React from 'react';
-import styles from './Copyright.module.scss';
+import React from 'react'
+import styles from './Copyright.module.scss'
 
 type Props = {
   copyright: string
-};
+}
 
 const Copyright = ({ copyright }: Props) => (
-  <div className={styles['copyright']}>
-    {copyright}
-  </div>
-);
+  <>
+    <div className={styles['copyright']}>{copyright}</div>
+    <cite className={styles['attribution']}>
+      Site theme was adapted from&nbsp;
+      <a
+        href='https://github.com/alxshelepenok/gatsby-starter-lumen'
+        rel='noopener noreferrer'
+      >
+        Lumen
+      </a>{' '}
+      by&nbsp;
+      <a href='https://github.com/alxshelepenok' rel='noopener noreferrer'>
+        Alexander Shelepenok
+      </a>
+      . Powered by&nbsp;
+      <a href='https://gatsbyjs.org' rel='noopener noreferrer'>
+        Gatsby
+      </a>
+      &nbsp;on&nbsp;
+      <a
+        href='https://aws.amazon.com/amplify/console'
+        rel='noopener noreferrer'
+      >
+        AWS Amplify
+      </a>
+      .
+    </cite>
+  </>
+)
 
-export default Copyright;
+export default Copyright
