fcf9c6b27d1c68167edf4434c704b9375fd553d7
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-04-11 23:23:55 +0800

    Adds KaTeX container CSS corrections.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 'use strict'
 
 require('./src/assets/scss/init.scss')
+require('./src/assets/scss/katex-extensions.scss')
 require('prism-themes/themes/prism-material-dark.css')
 require('./src/assets/scss/prismjs-extensions.scss')
